b93b7eda9068e85122659b9c601ad4b948fcba37
knot126 <cddepppp256@proton.me>
2026-07-18 18:36:08 +0000

    doc theme update

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -3,13 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from pathlib import Path
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'YipLoader'
 copyright = '2026, knot126'
 author = 'knot126'
-release = '0.1.0'
+release = Path('../RELEASE').read_text().strip()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,10 +21,11 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+primary_domain = 'c'
 highlight_language = 'c'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
+html_theme = 'furo'
 html_static_path = ['static']
